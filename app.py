924386e275f8d8e161c1fddbbda20dc25555db9e
subhadip1789 <fverrss@gmail.com>
2024-12-23 13:50:14 +0530

    Use Waitress for production

--- app.py
+++ app.py
@@ -58,9 +58,3 @@ def chat():
     user_message_count[user_ip] += 1
 
     return jsonify({"answer": answer})
-
-if __name__ == '__main__':
-    # Get the PORT environment variable provided by Render
-    port = int(os.environ.get("PORT", 5000))
-    # Bind the app to 0.0.0.0 and the provided port
-    app.run(host='0.0.0.0', port=port)
